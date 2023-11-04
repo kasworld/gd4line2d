7d72eab2fa95aba8e18ad9f2e5c396f4f85651c3
SeukWon Kang <kasworld@gmail.com>
2023-11-04 15:10:09 +0900

    multi moveline, youtube link

--- main.gd
+++ main.gd
@@ -2,16 +2,21 @@ extends Node2D
 
 var line_scene = preload("res://move_line2d/move_line_2d.tscn")
 
-const mv_ln_count = 1
+const mv_ln_count = 4
 var mv_ln_list :Array
 func _ready() -> void:
 	var draw_area = get_viewport_rect().size
 	for i in mv_ln_count:
-		make_mv_ln(draw_area)
+		make_mv_ln(600,2,draw_area/2)
+	mv_ln_list[1].position.x = draw_area.x/2
+	mv_ln_list[2].position.y = draw_area.y/2
+	mv_ln_list[3].position.x = draw_area.x/2
+	mv_ln_list[3].position.y = draw_area.y/2
 
-func make_mv_ln(draw_area :Vector2):
+
+func make_mv_ln(line_count :int, point_count:int, draw_area :Vector2):
 	var mvln = line_scene.instantiate()
-	mvln.init(600,2,draw_area)
+	mvln.init(line_count,point_count,draw_area)
 	add_child(mvln)
 	mv_ln_list.append(mvln)
 
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # 8bit 시절에 만들고 놀았던 라인그래픽을 현재의 우월한 해상도, 우월한 칼라, 우월한 성능을 사용해서 만들어본 소품입니다. 
 
 ![screenshot](doc/gd4line2d.png)
+
+https://www.youtube.com/watch?v=7toPKxIjk6k
